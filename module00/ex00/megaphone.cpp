a8c273f5252825e5e49294a87432b2888ced3cd0
Caniro <caniro@naver.com>
2021-02-04 22:58:30 +0900

    20210204 module00 ex00 - I don't know why I wrote the same variable name...

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: yuhan <yuhan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 05:03:58 by yuhan             #+#    #+#             */
-/*   Updated: 2021/02/02 05:51:13 by yuhan            ###   ########.fr       */
+/*   Updated: 2021/02/04 22:53:55 by yuhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int		main(int argc, char *argv[])
 		while (argv[++i])
 		{
 			std::string str = argv[i];
-			for (size_t i = 0; i < str.size(); ++i)
-				str[i] = toupper(str[i]);
+			for (size_t j = 0; j < str.size(); ++j)
+				str[j] = toupper(str[j]);
 			std::cout << str;
 		}
 		std::cout << std::endl;
